7baf79f62c51f6dc26e6d39672ae51b4e0bb3375
liuzhenhai <liuzhenhai@theduapp.com>
2019-01-04 11:21:53 +0800

    zipkin

---
+++ demo/src/test/java/com/spring/zipkin/ZipkindemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.spring.zipkin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZipkindemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
