90ab6017b8f05169938c898c6b1e79670b031655
liuzhenhai <liuzhenhai@theduapp.com>
2019-01-04 11:31:53 +0800

    zipkin

--- demo/src/main/java/com/spring/zipkin/ZipkindemoApplication.java
+++ demo/src/main/java/com/spring/zipkin/ZipkindemoApplication.java
@@ -2,8 +2,10 @@ package com.spring.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableZipkinServer
 public class ZipkindemoApplication {
 
 	public static void main(String[] args) {
